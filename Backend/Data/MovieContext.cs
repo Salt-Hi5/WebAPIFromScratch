a47baa096eb1deba8b7fa6ea1458023df029fbaf
Gelbrekt <john.ev.forsgren@gmail.com>
2023-03-09 16:21:51 +0100

    second commit

---
+++ Backend/Data/MovieContext.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+    public class MovieContext : DbContext
+    {
+        public MovieContext (DbContextOptions<MovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; } = default!;
+    }
---
+++ Backend/Models/Movie.cs
@@ -0,0 +1,11 @@
+
+
+public class Movie
+{
+
+    public int Id {get; set; } // The database will automatically detect a variable with "id" in its name to use for the id. 
+    public string Name {get; set; }
+    public string Year {get; set; }
+
+
+}
